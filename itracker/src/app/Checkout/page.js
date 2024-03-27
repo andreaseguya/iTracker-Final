b1b354b3a658a60860a23e0407dac759177fdd6d
Andrea Seguya <akseguya@gmail.com>
2024-03-26 19:27:21 -0700

    Added some changes to search and added material tailwinds

--- itracker/src/app/Checkout/page.js
+++ itracker/src/app/Checkout/page.js
@@ -1,6 +1,6 @@
 "use client"
 import Link from "next/link"
-
+import Search from "../search"
 
 export default function Check() {
     return (
@@ -14,6 +14,9 @@ export default function Check() {
                 <Link href="./CheckIn" class="basis-1/2 hover:border-b-4 radio">
                     <p>Check in</p>
                 </Link>
+            </div>
+            <div class="mt-5">
+                <Search />
             </div>
         </section>
     )
--- itracker/src/app/search.js
+++ itracker/src/app/search.js
@@ -1,7 +1,57 @@
+import axios from "axios"
+import { useState } from "react";
+import { Input } from "@material-tailwind/react";
+
 export default function Search() {
     return (
         <div>
-            <input type="search"></input>
+            <input type="search" class=""></input>
         </div>
     )
+}
+
+const SearchBar = () => {
+    const [query, setQuery] = useState("");
+
+    const handleSearch = async () => {
+        const response = await axios.get(`https://65f8f806df151452461037b3.mockapi.io/Asset`, {
+            params: {
+                query,
+            },
+        });
+
+        setQuery(response.data);
+        console.log(setQuery);
+    };
+
+    return (
+        <div>
+            <form>
+                <Input
+                    label="Search"
+                    variant="static" placeholder="Static"
+                    className=''
+                    type="search"
+                    value={query}
+                    onChange={(e) => setQuery(e.target.value)}
+                />
+
+                <button onClick={handleSearch}>Search</button>
+            </form>
+
+            <div>
+                {query.map((result) => (
+                    <div key={result.ID}>
+
+                        <p>{result.assetName}</p> </div>
+                ))}
+
+            </div>
+
+        </div>
+    );
+};
+
+function comb() {
+
 }
