6cbb3594e87b5a8b1a3ecd496f8c71171e593722
Andrea Seguya <akseguya@gmail.com>
2024-04-01 22:43:59 -0700

    some work on improving assetList

--- itracker/src/app/AssetDetails/page.js
+++ itracker/src/app/AssetDetails/page.js
@@ -1,26 +1,71 @@
 "use client"
-import axios from "axios";
-import { useState, useEffect } from "react";
-export default function Asset() {
-    const client = axios.create({
-        baseURL: `https://65f8f806df151452461037b3.mockapi.io/Asset`
-    });
-    const [assetList, setAssetList] = useState([]);
-    const [id, setID] = useState(0);
-
-    const fetchAssetById = async (id) => {
-        useEffect(() => {
-            client.get(`/${id}`).then((response) => {
-                setAssetList(response.data)
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+export default function AssetHelper() {
+    const [APIData, setAPIData] = useState([]);
+    const [id, setId] = useState(3);
+    useEffect(() => {
+        axios
+            .get("https://65f8f806df151452461037b3.mockapi.io/Asset")
+            .then(({ data }) => {
+                setAPIData(data);
             })
-        }, [])
-        return assetList;
-    }
-    const account = fetchAssetById(3);
-    console.log(account);
+            .catch((error) => {
+                console.log(error);
+            });
+    }, []);
+    const AssetDisplay = () => {
+        return APIData.map((res, i) => {
+            return <Assets
+                obj={res} key={i} />;
+        });
+    };
+    const DisplayByID = (id) => {
+        // console.log(APIData.filter(ID, id));
+        // return (
+        //     APIData.filter((item))=>{
+
+        //     }
+
+        // )
+    };
+
     return (
         <section>
+            {AssetDisplay()}
+        </section>
+    )
+}
 
+const Assets = (props) => {
+    const [singleAsset] = useState([]);
+    const {
+        _id,
+        assetName,
+        AlertEmail,
+        Quantity,
+        minQuantity,
+        status,
+        Returnable,
+        StorageLocation
+    } = props.obj;
+
+
+    return (
+        <section>
+            <div id="AssetHeader">
+                <div class="rounded-[50%] bg-gray-500 w-[50px] h-[50px]">.
+                </div>
+                <div class="" _id={1}>
+                    <h1>{assetName}</h1>
+                    <p>Last modified by ....</p>
+                    <button>Edit</button>
+                    <button>Delete</button>
+                </div>
+            </div>
+            <div>
+
+            </div>
         </section>
     )
 }
