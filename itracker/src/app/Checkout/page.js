0dbb848bb3d95febe0d6184b08ab0a92625296d4
Andrea Seguya <akseguya@gmail.com>
2024-05-13 15:26:13 -0700

    a

--- itracker/src/app/Checkout/page.js
+++ itracker/src/app/Checkout/page.js
@@ -1,5 +1,6 @@
 "use client"
-import SearchBar from "../AssetSearch/page";
+// import SearchBar from "../AssetSearch/page";
+import React, { Component } from "react";
 import { Combobox, ComboboxInput, ComboboxOption, ComboboxOptions } from '@headlessui/react'
 import { useState, useEffect } from 'react'
 import { Tab, TabGroup, TabList, TabPanel, TabPanels } from '@headlessui/react'
@@ -30,8 +31,9 @@ export default function Check() {
                             </Tab>
                         </TabList>
                         <TabPanels>
-                            <TabPanel><checkOut /></TabPanel>
-                            <TabPanel><Search /></TabPanel>
+                            <TabPanel><section>
+                            </section></TabPanel>
+                            <TabPanel>2</TabPanel>
 
                         </TabPanels>
                     </TabGroup>
@@ -42,9 +44,9 @@ export default function Check() {
         </section>
     )
 }
-function Search() {
 
-}
+
+
 function checkOut(props) {
     const {
         id,
@@ -58,16 +60,73 @@ function checkOut(props) {
     } = props.obj;
     return (
         <section>
+            <p class="text-black">2</p>
             <form>
 
             </form>
         </section>
     )
 }
+
 function checkIn(props) {
     return (
         <section>
-
+            <p class="text-black">2</p>
         </section>
     )
+}
+
+class SearchBar extends Component {
+    state = {
+        assets: [],
+        search: "",
+        loading: false
+
+    };
+
+    async componentDidMount() {
+        const { data: assets } = await api.get('/assets'
+        );
+        this.setState({ assets });
+    }
+    searchChanged = event => {
+        this.setState({ search: event.target.value })
+    }
+    render() {
+        return (
+            <div class="">
+                <input
+                    class="  w-[350px]  bg-gray-100 text-blue-gray-700 font-inter font-normal outline outline-0 focus:outline-0 disabled:bg-blue-gray-50 disabled:border-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 border focus:border-1  text-sm px-3 py-2.5 rounded-[7px]  focus:border-gray-900"
+                    type='text' placeholder="Search by name" onChange={this.searchChanged} value={this.state.search} />
+
+                <div class=" mt-3 w-[370px] flex flex-row gap-3 flex-wrap">
+                    {this.state.assets
+                        .filter((asset) => {
+                            if (this.state.search == "") {
+                                return "";
+                            }
+                            else if (asset.assetName.toLowerCase().includes(this.state.search.toLowerCase()))
+                                return asset
+
+                        })
+                        .map((asset) => {
+                            if (asset.assetName.toLowerCase().includes(this.state.search.toLowerCase())) {
+                                return (
+                                    <div key={asset.id} class="">
+                                        <div class="bg-gray-200 rounded-[50%] w-[70px] h-[70px] ">
+                                            {/* <Selected />  */}
+                                        </div>
+                                        <h1 class="ml-3 mt-0.5 w-[70px]">{asset.assetName}</h1>
+                                    </div>
+                                )
+                            }
+                        }
+
+                        )
+
+                    }
+                </div>
+            </div>
+        );
+    }
 }
