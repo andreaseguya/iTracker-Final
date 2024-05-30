c5b4a9e2f2cd332326bab3f0950393649d071fd9
Andrea Seguya <akseguya@gmail.com>
2024-05-30 15:57:58 -0700

    started check in

--- itracker/src/app/Checkout/checkIn.js
+++ itracker/src/app/Checkout/checkIn.js
@@ -1,7 +1,70 @@
+import {useState,useEffect} from 'react'
 export default function CheckIn() {
     return (
         <section>
-            3
+            <p>Scan or enter asset tags to quickly return items</p>
+            <Search/>
         </section>
+    )
+}
+
+function Search({ searchC, CSF, addAtoC }) {
+    const [APIData, setAPIData] = useState([]);
+    useEffect(() => {
+        const fetchAssets = async () => {
+            try {
+                const response = await api.get('/assets');
+                setAPIData(response.data);
+            } catch (err) {
+                if (err.response) {
+                    console.log(err.response.data);
+                    console.log(err.response.status);
+                    console.log(err.response.headers);
+                } else {
+                    console.log(`Error: ${err.message}`);
+                }
+            }
+        }
+        fetchAssets();
+    }, [])
+    return (
+        <div>
+            <input
+                type="text"
+                class="w-[300px]  bg-[rgba(151,151,151,0.14)] text-blue-gray-700 font-inter font-normal outline outline-0 focus:outline-0 disabled:bg-blue-gray-50 disabled:border-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 border focus:border-1  text-sm px-3 py-2.5 rounded-[7px]  focus:border-gray-900"
+                placeholder="Search with keywords or scan barcode"
+                value={searchC}
+                onChange={CSF}
+            />
+            <div class=" mt-3 w-[300px] flex flex-row gap-3 flex-wrap">
+                {APIData
+                    .filter((asset) => {
+                        if (searchC == "") {
+                            return "";
+                        }
+                        else if (asset.assetName.toLowerCase().includes(searchC.toLowerCase()))
+                            return asset
+
+                    })
+                    .map((asset) => {
+                        if (asset.assetName.toLowerCase().includes(searchC.toLowerCase())) {
+                            return (
+                                <div key={asset.id} class="">
+                                    <div class="bg-gray-200 rounded-[50%] w-[70px] h-[70px] "> </div>
+                                    <h1 class="ml-3 mt-0.5 w-[70px]">{asset.assetName}</h1>
+                                    <button
+                                        className="mt-3 ml-2 w-[50px] text-white rounded-[5px] bg-black hover:bg-red-500 p-1"
+                                        onClick={() => addAtoC(asset)}
+                                    >
+                                        Add
+                                    </button>
+                                </div>
+                            )
+                        }
+                    }
+                    )
+                }
+            </div>
+        </div>
     )
 }
