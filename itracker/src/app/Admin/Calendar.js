6485407ba925fff32f87000a3836dedfd2b6689c
Andrea Seguya <akseguya@gmail.com>
2024-07-02 08:30:19 -0700

    as
    as

--- itracker/src/app/Admin/Calendar.js
+++ itracker/src/app/Admin/Calendar.js
@@ -1,12 +1,7 @@
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native-web'
-import moment from 'moment'
-export default function Calendar() {
-    const Date = ({ date, onSelectDate, selected }) => {
-        const day = moment(date).format('YYYY-MM-DD') === moment().format('YYYY-MM-DD') ? 'Today' : moment(date).format('ddd')
-    }
-    return (
-        <div class="mt-1 ml-2">
+"use client"
+import React, { Component } from 'react';
+export default class Calendar extends Component {
+    constructor() {
 
-        </div>
-    )
+    }
 }
