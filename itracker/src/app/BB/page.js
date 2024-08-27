05bc1fde82ffb13742a59bdc577f7df929f0918f
Andrea Seguya <akseguya@gmail.com>
2024-08-27 08:11:27 -0700

    AS

--- itracker/src/app/BB/page.js
+++ itracker/src/app/BB/page.js
@@ -8,10 +8,24 @@ import interactionPlugin from '@fullcalendar/interaction'
 export default function Scheduler() {
     const [weekendsVisible, setWeekendsVisible] = useState(true)
     const [currentEvents, setCurrentEvents] = useState([])
+    // const [appointments, setAppointments] = useState([])
     function formatEvents() {
-        return (
-            <div></div>
-        )
+        return currentEvents.map(appointment => {
+            const { title, end, start } = appointment
+
+            let startTime = new Date(start)
+            let endTime = new Date(end)
+
+            return {
+                title,
+                start: startTime,
+                end: endTime,
+                extendedProps: { ...appointment }
+            }
+        })
+    }
+    function handleEventClick({ event }) {
+
     }
     return (
         <section class="w-[400px]">
@@ -30,7 +44,7 @@ export default function Scheduler() {
                 weekends={weekendsVisible}
                 // 8/26
                 // eventDrop={this.handleEventDrop}
-                // eventClick={this.handleEventClick}
+                eventClick={handleEventClick}
                 events={formatEvents()}
 
             //previous paste
@@ -43,6 +57,23 @@ export default function Scheduler() {
             eventAdd={function(){}}
             eventChange={function(){}}
             eventRemove={function(){}}
+//8/27
+            handleEventClick= ({event}) => {
+    // openAppointment is a function I wrote to open a form to edit that appointment
+    this.props.openAppointment(event.extendedProps)
+}
+
+handleEventDrop = (info) => {
+        if(window.confirm("Are you sure you want to change the event date?")){
+            console.log('change confirmed')
+
+            // updateAppointment is another custom method
+            this.props.updateAppointment({...info.event.extendedProps, start: info.event.start, end: info.event.end})
+
+        } else {
+            console.log('change aborted')
+        }
+   }
             */
             />
 
