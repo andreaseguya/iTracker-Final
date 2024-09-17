ba64f1afd64a2a0e8d811535e06f742896b62e29
Andrea Seguya <akseguya@gmail.com>
2024-09-17 08:25:23 -0700

    as
    as

--- itracker/src/app/BB/page.js
+++ itracker/src/app/BB/page.js
@@ -5,26 +5,27 @@ import FullCalendar from '@fullcalendar/react'
 import dayGridPlugin from '@fullcalendar/daygrid'
 import timeGridPlugin from '@fullcalendar/timegrid'
 import interactionPlugin, { Draggable } from '@fullcalendar/interaction'
+import { Calendar } from '@fullcalendar/core'
 
-export default function Scheduler() {
+function Scheduler() {
     const [weekendsVisible, setWeekendsVisible] = useState(true)
     const [currentEvents, setCurrentEvents] = useState([])
     // const [appointments, setAppointments] = useState([]
-    function formatEvents() {
-        return currentEvents.map(appointment => {
-            const { title, end, start } = appointment
+    // function formatEvents() {
+    //     return currentEvents.map(appointment => {
+    //         const { title, end, start } = appointment
 
-            let startTime = new Date(start)
-            let endTime = new Date(end)
+    //         let startTime = new Date(start)
+    //         let endTime = new Date(end)
 
-            return {
-                title,
-                start: startTime,
-                end: endTime,
-                extendedProps: { ...appointment }
-            }
-        })
-    }
+    //         return {
+    //             title,
+    //             start: startTime,
+    //             end: endTime,
+    //             extendedProps: { ...appointment }
+    //         }
+    //     })
+    // }
     function handleDateClick(e) {
         // alert(e.dateStr)
 
@@ -34,11 +35,18 @@ export default function Scheduler() {
 
             <FullCalendar
                 plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
-                droppable={true}
+                // droppable={true}
                 headerToolbar={{
                     left: 'prev,next',
                     // center: 'today',
                     right: 'dayGridMonth,timeGridWeek,timeGridDay'
+                }}
+                //custom button for edit
+                customButtons={{
+                    addEvent: {
+                        text: 'add event',
+                        click: () => { }
+                    }
                 }}
                 initialView='dayGridMonth'
                 editable={true}
@@ -49,7 +57,7 @@ export default function Scheduler() {
                 // 8/26
                 // eventDrop={this.handleEventDrop}
                 dateClick={handleDateClick}
-                events={formatEvents()}
+            // events={formatEvents()}
 
             //previous paste
             // initialEvents={INITIAL_EVENTS} // alternatively, use the `events` setting to fetch from a feed
@@ -85,3 +93,45 @@ handleEventDrop = (info) => {
     )
 }
 
+export default function CalenderTest() {
+    return (
+        <section class="w-[400px]">
+            <FullCalendar
+                plugins={[dayGridPlugin, interactionPlugin, timeGridPlugin]}
+                headerToolbar={{
+                    left: 'prev,next addEvent',
+                    // center: 'title',
+                    right: 'dayGridMonth,timeGridWeek,timeGridDay'
+                }}
+                footerToolbar={{
+                    // center: 'title',
+                }}
+                customButtons={{
+                    addEvent: {
+                        text: 'Reserve',
+                        click: () => {
+                            // var tempdate = prompt('Enter a date in YYYY-MM-DD format')
+                            // var date = new Date(tempdate + 'T00:00:00')
+                            // if (!isNaN(date.valueOf())) {
+                            //     calendar.addEvent({
+                            //         title: 'dynamic event',
+                            //         start: date,
+                            //         allDay: true
+                            //     })
+                            // }// valid date?
+                        }
+                    }
+                }}
+
+                initialView="dayGridMonth"
+                events={[
+                    {
+                        title: 'event 1',
+                        start: '2024-09-09T10:00:00',
+                        end: '2024-09-08T11:00:00',
+                    }
+                ]}
+            />
+        </section>
+    )
+}
