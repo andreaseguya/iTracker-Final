e72db1511e57c3a7293b94fc8be9646cb3ad18b3
Andrea Seguya <akseguya@gmail.com>
2024-09-16 23:44:26 -0700

    as

--- itracker/src/app/BB/page.js
+++ itracker/src/app/BB/page.js
@@ -1,6 +1,6 @@
 "use client"
 import React, { useState } from 'react'
-import { Calendar } from '@fullcalendar/core'
+// import { Calendar } from '@fullcalendar/core'
 import FullCalendar from '@fullcalendar/react'
 import dayGridPlugin from '@fullcalendar/daygrid'
 import timeGridPlugin from '@fullcalendar/timegrid'
